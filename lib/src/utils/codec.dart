d86d3d9c7f1383625f48ea0b35a5e31c38c396b0
tGautot <44877251+tGautot@users.noreply.github.com>
2019-09-29 18:51:54 +0200

    Updated package name

--- lib/src/utils/codec.dart
+++ lib/src/utils/codec.dart
@@ -1,4 +1,4 @@
-import 'package:permission_handler/src/permission_enums.dart';
+import 'package:media_permission_handler/src/permission_enums.dart';
 
 class Codec {
   static PermissionStatus decodePermissionStatus(int value) {
